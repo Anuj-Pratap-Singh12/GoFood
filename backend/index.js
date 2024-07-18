8fac7994a886dfd2f3738bde3c41428d8bbf3de7
Anuj-Pratap-Singh12 <anujpratap4595@gmail.com>
2024-07-18 15:34:10 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,14 +14,6 @@ app.use(cors(
 const mongodb = require("./db"); // Ensure db.js is imported to establish the database connection
 mongodb();
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 
 app.use(express.json());
 app.use("/api", require("./Routes/CreateUser"));
